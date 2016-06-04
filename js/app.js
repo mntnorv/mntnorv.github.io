7865a8406588f52b384f8b50bf5e9c923335c325
Mantas Norvaiša <mntnorv@gmail.com>
2016-06-04 22:08:45 +0300

    Track stuff

--- js/app.js
+++ js/app.js
@@ -2,13 +2,68 @@
   'use strict';
   
   var
-    canvas = document.getElementById('background'),
-    ctx    = canvas.getContext("2d");
+    canvas      = document.getElementById('background'),
+    ctx         = canvas.getContext("2d"),
+    trackColors = ['#333', '#444'],
+    trackPieces = [],
+    trackPieceL = 0.3,
+    i, j, k;
   
-  ctx.fillStyle = "#FF0000";
-  ctx.fillRect(0, 0, 150, 75);
+  function generateTrackPiece(i) {
+    return [
+      {
+        points: [
+          [-1, -1 + Math.pow((i + 1), -0.25) - 1, 1 + (i * trackPieceL)],
+          [ 1, -1 + Math.pow((i + 1), -0.25) - 1, 1 + (i * trackPieceL)],
+          [ 1, -1 + Math.pow((i + 2), -0.25) - 1, 1 + ((i + 1) * trackPieceL)],
+          [-1, -1 + Math.pow((i + 2), -0.25) - 1, 1 + ((i + 1) * trackPieceL)]
+        ],
+        color: trackColors[i % 2]
+      }
+    ];
+  }
+  
+  function projectVec3(vec3) {
+    var
+      halfH = canvas.height / 2,
+      halfW = canvas.width  / 2;
+    
+    return [
+      ((vec3[0] / vec3[2]) + 1) * halfW,
+      canvas.height - (((vec3[1] / vec3[2]) + 1) * halfH)
+    ];
+  }
+  
+  for (i = 0; i < 10; i += 1) {
+    trackPieces.push(generateTrackPiece(i));
+  }
 
   function render() {
+    for (i = 0; i < trackPieces.length; i += 1) {
+      var
+        piece = trackPieces[i],
+        component, point;
+      
+      for (j = 0; j < piece.length; j += 1) {
+        component = piece[j];
+        
+        ctx.fillStyle = component.color;
+        ctx.beginPath();
+        
+        for (k = 0; k < component.points.length; k += 1) {
+          point = projectVec3(component.points[k]);
+          if (k === 0) {
+            ctx.moveTo(point[0], point[1]);
+          } else {
+            ctx.lineTo(point[0], point[1]);
+          }
+        }
+        
+        ctx.closePath();
+        ctx.fill();
+      }
+    }
+    
     window.requestAnimationFrame(render);
   }
   
