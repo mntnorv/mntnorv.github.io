fff17551d61a93cc2444a713dc6ec0b36bc06edc
Mantas Norvaiša <mntnorv@gmail.com>
2016-06-14 23:14:29 +0300

    Pixelated canvas stuff

--- js/app.js
+++ js/app.js
@@ -1,279 +1,30 @@
-/// <reference path="gl.ts"/>
 var App;
 (function (App) {
-    var TRACK_PIECE_LENGTH = 0.3;
-    var SPEED = 0.1;
-    var MAX_PIECES_VISIBLE = 50;
-    var STEPS_PER_PIECE = TRACK_PIECE_LENGTH / SPEED;
-    var GRASS_COLORS = [
-        [0, 0.443, 0],
-        [0, 0.349, 0]
-    ];
-    var TRACK_COLORS = [
-        [0.2, 0.2, 0.2],
-        [0.267, 0.267, 0.267]
-    ];
-    var SIDE_COLORS = [
-        [0.8, 0, 0],
-        [0.867, 0.867, 0.867]
-    ];
-    var TRACK_PIECE_ODD_FEATURES = [
-        {
-            width: 0.05,
-            color: SIDE_COLORS[1]
-        }, {
-            width: 0.425,
-            color: TRACK_COLORS[1]
-        }, {
-            width: 0.05,
-            color: [0.867, 0.867, 0.867]
-        }, {
-            width: 0.425,
-            color: TRACK_COLORS[1]
-        }, {
-            width: 0.05,
-            color: SIDE_COLORS[1]
-        }
-    ];
-    var TRACK_PIECE_EVEN_FEATURES = [
-        {
-            width: 0.05,
-            color: SIDE_COLORS[0]
-        }, {
-            width: 0.9,
-            color: TRACK_COLORS[0]
-        }, {
-            width: 0.05,
-            color: SIDE_COLORS[0]
-        }
-    ];
-    var TrackGenerationFeature = (function () {
-        function TrackGenerationFeature() {
-        }
-        return TrackGenerationFeature;
-    }());
-    var TrackGenerationFeatures = (function () {
-        function TrackGenerationFeatures() {
-        }
-        return TrackGenerationFeatures;
-    }());
-    var TrackFeature = (function () {
-        function TrackFeature() {
-        }
-        return TrackFeature;
-    }());
-    var TrackPiece = (function () {
-        function TrackPiece() {
-        }
-        return TrackPiece;
-    }());
-    var Polygon = (function () {
-        function Polygon() {
-        }
-        return Polygon;
-    }());
-    var TrackOffsets = (function () {
-        function TrackOffsets() {
-        }
-        return TrackOffsets;
-    }());
-    var realCanvas, ctx, canvas, trackFeatures, gl, shaderProgram, positionLocation, colorLocation, projectionLocation, buffer, paused = false, track = [], camera = [0, 0, 0], stepsMoved = 0, pointsArray = new Float32Array(12), i;
+    var MIN_WIDTH = 240;
+    var MIN_HEIGHT = 135;
+    var realCanvas, realCtx, canvas, ctx, paused = false;
     function resizeCanvas() {
-        canvas.width = Math.floor(window.innerWidth / 4);
-        canvas.height = Math.floor(window.innerHeight / 4);
-        gl.viewport(0, 0, canvas.width, canvas.height);
+        var pixelRatio;
+        pixelRatio = Math.min(window.innerWidth / MIN_WIDTH, window.innerHeight / MIN_HEIGHT);
+        canvas.width = Math.ceil(window.innerWidth / pixelRatio);
+        canvas.height = Math.ceil(window.innerHeight / pixelRatio);
+        console.log(pixelRatio, canvas.width, canvas.height);
         realCanvas.width = window.innerWidth;
         realCanvas.height = window.innerHeight;
-        ctx.mozImageSmoothingEnabled = false;
-        ctx.webkitImageSmoothingEnabled = false;
-        ctx.msImageSmoothingEnabled = false;
-        ctx.imageSmoothingEnabled = false;
+        realCtx.mozImageSmoothingEnabled = false;
+        realCtx.webkitImageSmoothingEnabled = false;
+        realCtx.msImageSmoothingEnabled = false;
+        realCtx.imageSmoothingEnabled = false;
+        ctx.translate(0.5, 0.5);
     }
-    function generateTrackFeature(current, max, min) {
-        var sign;
-        if (current.steps > current.stepsDone) {
-            return current;
-        }
-        if (Math.random() < 0.5) {
-            sign = 1;
-        }
-        else {
-            sign = -1;
-        }
-        switch (Math.floor(Math.random() * 2)) {
-            case 0:
-                return {
-                    change: 0,
-                    steps: Math.floor(Math.random() * 10) + 10,
-                    stepsDone: 0
-                };
-            case 1:
-                return {
-                    steps: Math.floor(Math.random() * 15) + 15,
-                    stepsDone: 0,
-                    change: ((Math.random() * (max - min)) + min) * sign
-                };
-        }
-    }
-    function generateTrackPiece(even, trackFeatures) {
-        var elevation, features;
-        trackFeatures.hill = generateTrackFeature(trackFeatures.hill, 1.5, 2.5);
-        trackFeatures.turn = generateTrackFeature(trackFeatures.turn, 0.01, 0.03);
-        elevation = interpolateTrackElevation(trackFeatures.hill);
-        if (even) {
-            features = TRACK_PIECE_EVEN_FEATURES;
-        }
-        else {
-            features = TRACK_PIECE_ODD_FEATURES;
-        }
-        trackFeatures.hill.stepsDone += 1;
-        trackFeatures.turn.stepsDone += 1;
-        return {
-            elevation: elevation,
-            turn: trackFeatures.turn.change,
-            background: GRASS_COLORS[even ? 0 : 1],
-            features: features,
-            even: even
-        };
-    }
-    function transformToCamera(point) {
-        var newPoint = point.slice(0);
-        if (typeof point[0] == 'number') {
-            newPoint[0] -= camera[0];
-        }
-        newPoint[1] -= camera[1];
-        newPoint[2] -= camera[2];
-        return newPoint;
-    }
-    function interpolateCameraY(cameraZ, offsets) {
-        var interpolationCoef, bottomPieceIdx, bottomPiece, bottomPieceY, i, cameraY;
-        interpolationCoef = (cameraZ % TRACK_PIECE_LENGTH) / TRACK_PIECE_LENGTH;
-        bottomPieceIdx = Math.floor(cameraZ / TRACK_PIECE_LENGTH);
-        bottomPiece = track[bottomPieceIdx];
-        bottomPieceY = 0;
-        for (i = 0; i < bottomPieceIdx; i += 1) {
-            bottomPieceY += track[i].elevation - offsets.elevation;
-        }
-        cameraY = (bottomPiece.elevation - offsets.elevation) * interpolationCoef + bottomPieceY + 1;
-        return cameraY;
-    }
-    function interpolateTrackElevation(hillFeature) {
-        return (Math.sin((Math.PI *
-            ((hillFeature.stepsDone + 1) / hillFeature.steps)) - (Math.PI / 2)) * hillFeature.change) - (Math.sin((Math.PI *
-            (hillFeature.stepsDone / hillFeature.steps)) - (Math.PI / 2)) * hillFeature.change);
-    }
-    function interpolateElevationOffset(last, now, cameraZ) {
-        var interpolationCoef = (cameraZ % TRACK_PIECE_LENGTH) / TRACK_PIECE_LENGTH;
-        return ((now - last) * interpolationCoef + last);
-    }
-    function build3DTrack(track, offsets) {
-        var threeDTrack = [], combinedElevation = 0, combinedTurn = 0, combinedXOffset = 0, piece, feature, featureXOffset, i, j, y1, y2, z1, z2, x1Offset, x2Offset;
-        combinedTurn += offsets.turn;
-        combinedXOffset += offsets.turn;
-        for (i = 0; i < track.length; i += 1) {
-            combinedElevation += track[i].elevation - offsets.elevation;
-            if (i > Math.floor(stepsMoved / STEPS_PER_PIECE)) {
-                combinedTurn += track[i].turn;
-                combinedXOffset += combinedTurn;
-            }
-        }
-        for (i = (track.length - 1); i >= 0; i -= 1) {
-            piece = track[i];
-            x1Offset = combinedXOffset - combinedTurn;
-            x2Offset = combinedXOffset;
-            y1 = combinedElevation - (piece.elevation - offsets.elevation);
-            y2 = combinedElevation;
-            z1 = i * TRACK_PIECE_LENGTH + 1;
-            z2 = (i + 1) * TRACK_PIECE_LENGTH + 1;
-            // Background
-            threeDTrack.push({
-                points: [
-                    [-1000, y1, z1],
-                    [1000, y1, z1],
-                    [1000, y2, z2],
-                    [-1000, y2, z2]
-                ],
-                color: piece.background
-            });
-            // Features
-            featureXOffset = 0;
-            for (j = 0; j < piece.features.length; j += 1) {
-                feature = piece.features[j];
-                threeDTrack.push({
-                    points: [
-                        [
-                            -1 + (featureXOffset * 2) + x1Offset,
-                            y1, z1
-                        ], [
-                            -1 + ((featureXOffset + feature.width) * 2) + x1Offset,
-                            y1, z1
-                        ], [
-                            -1 + ((featureXOffset + feature.width) * 2) + x2Offset,
-                            y2, z2
-                        ], [
-                            -1 + (featureXOffset * 2) + x2Offset,
-                            y2, z2
-                        ]
-                    ],
-                    color: feature.color
-                });
-                featureXOffset += feature.width;
-            }
-            combinedElevation -= (piece.elevation - offsets.elevation);
-            combinedXOffset -= combinedTurn;
-            combinedTurn -= piece.turn;
-        }
-        return threeDTrack;
-    }
-    function update() {
-        var invisiblePieces, piecesToRemove, i, lastPieceEven, offsets;
-        // Advance position
-        stepsMoved += 1;
-        camera[2] = stepsMoved * SPEED;
-        // Remove old track pieces, add new ones
-        piecesToRemove = Math.floor(stepsMoved / STEPS_PER_PIECE) - 2;
-        lastPieceEven = track[track.length - 1].even;
-        if (piecesToRemove > 0) {
-            track.splice(0, piecesToRemove);
-            for (i = 0; i < piecesToRemove; i += 1) {
-                lastPieceEven = !lastPieceEven;
-                track.push(generateTrackPiece(lastPieceEven, trackFeatures));
-            }
-            stepsMoved -= piecesToRemove * STEPS_PER_PIECE;
-            camera[2] = stepsMoved * SPEED;
-        }
-        offsets = {
-            elevation: 0,
-            turn: 0
-        };
-        // Update elevation offset
-        offsets.elevation = interpolateElevationOffset((track[0].elevation + track[1].elevation + track[2].elevation + track[3].elevation + track[4].elevation) / 5, (track[1].elevation + track[2].elevation + track[3].elevation + track[4].elevation + track[5].elevation) / 5, camera[2]);
-        // Update turn offset
-        invisiblePieces = Math.floor(stepsMoved / STEPS_PER_PIECE);
-        offsets.turn = (1 - ((stepsMoved % STEPS_PER_PIECE) / STEPS_PER_PIECE)) * track[invisiblePieces].turn;
-        // Interpolate camera Y
-        camera[1] = interpolateCameraY(camera[2], offsets);
-        // Build 3D track
-        return build3DTrack(track, offsets);
-    }
-    function render(polygons) {
-        var polygon, point, i, j;
-        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-        for (i = 0; i < polygons.length; i += 1) {
-            polygon = polygons[i];
-            for (j = 0; j < polygon.points.length; j += 1) {
-                point = transformToCamera(polygon.points[j].slice(0));
-                pointsArray[j * 3] = point[0];
-                pointsArray[j * 3 + 1] = point[1];
-                pointsArray[j * 3 + 2] = point[2];
-            }
-            gl.uniform4f(colorLocation, polygon.color[0], polygon.color[1], polygon.color[2], 1);
-            gl.uniformMatrix4fv(projectionLocation, false, GL.perspectiveMatrix());
-            gl.bufferData(gl.ARRAY_BUFFER, pointsArray, gl.STATIC_DRAW);
-            gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
-        }
-        ctx.clearRect(0, 0, realCanvas.width, realCanvas.height);
-        ctx.drawImage(canvas, 0, 0, canvas.width, canvas.height, 0, 0, realCanvas.width, realCanvas.height);
+    function render() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.strokeStyle = '#000';
+        ctx.moveTo(10, 10);
+        ctx.lineTo(10, 20);
+        ctx.stroke();
+        realCtx.clearRect(0, 0, realCanvas.width, realCanvas.height);
+        realCtx.drawImage(canvas, 0, 0, canvas.width, canvas.height, 0, 0, realCanvas.width, realCanvas.height);
     }
     function playPause() {
         paused = !paused;
@@ -283,9 +34,7 @@ var App;
     }
     App.playPause = playPause;
     function nextFrame() {
-        var threeDTrack;
-        threeDTrack = update();
-        render(threeDTrack);
+        render();
         if (!paused) {
             window.requestAnimationFrame(nextFrame);
         }
@@ -293,27 +42,9 @@ var App;
     App.nextFrame = nextFrame;
     // Initialize stuff
     realCanvas = document.getElementById('game');
-    ctx = realCanvas.getContext('2d');
+    realCtx = realCanvas.getContext('2d');
     canvas = document.createElement('canvas');
-    gl = GL.initWebGL(canvas);
-    shaderProgram = GL.initShaders(gl);
-    positionLocation = gl.getAttribLocation(shaderProgram, 'a_position');
-    colorLocation = gl.getUniformLocation(shaderProgram, 'u_color');
-    projectionLocation = gl.getUniformLocation(shaderProgram, 'u_projection');
-    buffer = gl.createBuffer();
-    gl.clearColor(0, 0, 0, 0);
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-    gl.enableVertexAttribArray(positionLocation);
-    gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
-    // Initial track features
-    trackFeatures = {
-        hill: { steps: 0, stepsDone: 0, change: 0 },
-        turn: { steps: 0, stepsDone: 0, change: 0 }
-    };
-    // Initial track
-    for (i = 0; i < MAX_PIECES_VISIBLE; i += 1) {
-        track.push(generateTrackPiece(i % 2 === 0, trackFeatures));
-    }
+    ctx = canvas.getContext('2d');
     // Resize the canvas to fill browser window dynamically
     window.addEventListener('resize', resizeCanvas, false);
     // Start
--- src/app.ts
+++ src/app.ts
@@ -1,406 +1,49 @@
-/// <reference path="gl.ts"/>
-
 namespace App {
-  const TRACK_PIECE_LENGTH = 0.3;
-  const SPEED = 0.1;
-  const MAX_PIECES_VISIBLE = 50;
-  const STEPS_PER_PIECE = TRACK_PIECE_LENGTH / SPEED;
-
-  const GRASS_COLORS = [
-    [0, 0.443, 0],
-    [0, 0.349, 0]
-  ];
-
-  const TRACK_COLORS = [
-    [0.2, 0.2, 0.2],
-    [0.267, 0.267, 0.267]
-  ];
-
-  const SIDE_COLORS = [
-    [0.8, 0, 0],
-    [0.867, 0.867, 0.867]
-  ];
-
-  const TRACK_PIECE_ODD_FEATURES = [
-    {
-      width: 0.05,
-      color: SIDE_COLORS[1]
-    }, {
-      width: 0.425,
-      color: TRACK_COLORS[1]
-    }, {
-      width: 0.05,
-      color: [0.867, 0.867, 0.867]
-    }, {
-      width: 0.425,
-      color: TRACK_COLORS[1]
-    }, {
-      width: 0.05,
-      color: SIDE_COLORS[1]
-    }
-  ];
-
-  const TRACK_PIECE_EVEN_FEATURES = [
-    {
-      width: 0.05,
-      color: SIDE_COLORS[0]
-    }, {
-      width: 0.9,
-      color: TRACK_COLORS[0]
-    }, {
-      width: 0.05,
-      color: SIDE_COLORS[0]
-    }
-  ];
-
-  class TrackGenerationFeature {
-    steps: number;
-    stepsDone: number;
-    change: number;
-  }
-
-  class TrackGenerationFeatures {
-    hill: TrackGenerationFeature;
-    turn: TrackGenerationFeature;
-  }
-
-  class TrackFeature {
-    color: number[];
-    width: number;
-  }
-
-  class TrackPiece {
-    elevation: number;
-    turn: number;
-    background: number[];
-    features: TrackFeature[];
-    even: boolean;
-  }
-
-  class Polygon {
-    color: number[];
-    points: number[][];
-  }
-
-  class TrackOffsets {
-    elevation: number;
-    turn: number;
-  }
+  const MIN_WIDTH  = 240;
+  const MIN_HEIGHT = 135;
 
   let
     realCanvas: HTMLCanvasElement,
-    ctx: CanvasRenderingContext2D,
+    realCtx: CanvasRenderingContext2D,
     canvas: HTMLCanvasElement,
-    trackFeatures: TrackGenerationFeatures,
-    gl: WebGLRenderingContext,
-    shaderProgram: WebGLProgram,
-    positionLocation: number,
-    colorLocation: WebGLUniformLocation,
-    projectionLocation: WebGLUniformLocation,
-    buffer: WebGLBuffer,
-    paused          = false,
-    track           = [] as TrackPiece[],
-    camera          = [0, 0, 0],
-    stepsMoved      = 0,
-    pointsArray     = new Float32Array(12),
-    i: number;
+    ctx: CanvasRenderingContext2D,
+    paused = false;
 
   function resizeCanvas() {
-    canvas.width  = Math.floor(window.innerWidth / 4);
-    canvas.height = Math.floor(window.innerHeight / 4);
-    gl.viewport(0, 0, canvas.width, canvas.height);
+    let pixelRatio: number;
+
+    pixelRatio = Math.min(
+      window.innerWidth  / MIN_WIDTH,
+      window.innerHeight / MIN_HEIGHT
+    );
+
+    canvas.width  = Math.ceil(window.innerWidth  / pixelRatio);
+    canvas.height = Math.ceil(window.innerHeight / pixelRatio);
+
+    console.log(pixelRatio, canvas.width, canvas.height);
 
     realCanvas.width  = window.innerWidth;
     realCanvas.height = window.innerHeight;
 
-    ctx.mozImageSmoothingEnabled = false;
-    ctx.webkitImageSmoothingEnabled = false;
-    ctx.msImageSmoothingEnabled = false;
-    ctx.imageSmoothingEnabled = false;
+    (realCtx as any).mozImageSmoothingEnabled = false;
+    (realCtx as any).webkitImageSmoothingEnabled = false;
+    (realCtx as any).msImageSmoothingEnabled = false;
+    (realCtx as any).imageSmoothingEnabled = false;
+
+    ctx.translate(0.5, 0.5);
   }
 
-  function generateTrackFeature(current: TrackGenerationFeature, max: number, min: number) {
-    let sign: number;
+  function render() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    if (current.steps > current.stepsDone) {
-      return current;
-    }
+    ctx.strokeStyle = '#000';
+    ctx.moveTo(10, 10);
+    ctx.lineTo(10, 20);
+    ctx.stroke();
 
-    if (Math.random() < 0.5) {
-      sign = 1;
-    } else {
-      sign = -1;
-    }
-
-    switch (Math.floor(Math.random() * 2)) {
-      case 0:
-        return {
-          change: 0,
-          steps: Math.floor(Math.random() * 10) + 10,
-          stepsDone: 0
-        };
-      case 1:
-        return {
-          steps: Math.floor(Math.random() * 15) + 15,
-          stepsDone: 0,
-          change: ((Math.random() * (max - min)) + min) * sign
-        };
-    }
-  }
-
-  function generateTrackPiece(even: boolean, trackFeatures: TrackGenerationFeatures): TrackPiece {
-    let elevation: number, features: TrackFeature[];
-
-    trackFeatures.hill = generateTrackFeature(trackFeatures.hill, 1.5, 2.5);
-    trackFeatures.turn = generateTrackFeature(trackFeatures.turn, 0.01, 0.03);
-
-    elevation = interpolateTrackElevation(trackFeatures.hill);
-
-    if (even) {
-      features = TRACK_PIECE_EVEN_FEATURES;
-    } else {
-      features = TRACK_PIECE_ODD_FEATURES;
-    }
-
-    trackFeatures.hill.stepsDone += 1;
-    trackFeatures.turn.stepsDone += 1;
-
-    return {
-      elevation: elevation,
-      turn: trackFeatures.turn.change,
-      background: GRASS_COLORS[even ? 0 : 1],
-      features: features,
-      even: even
-    };
-  }
-
-  function transformToCamera(point: number[]) {
-    let newPoint = point.slice(0) as number[];
-
-    if (typeof point[0] == 'number') {
-      newPoint[0] -= camera[0];
-    }
-
-    newPoint[1] -= camera[1];
-    newPoint[2] -= camera[2];
-
-    return newPoint;
-  }
-
-  function interpolateCameraY(cameraZ: number, offsets: TrackOffsets) {
-    let
-      interpolationCoef: number,
-      bottomPieceIdx: number,
-      bottomPiece: TrackPiece,
-      bottomPieceY: number,
-      i: number,
-      cameraY: number;
-
-    interpolationCoef = (cameraZ % TRACK_PIECE_LENGTH) / TRACK_PIECE_LENGTH;
-    bottomPieceIdx = Math.floor(cameraZ / TRACK_PIECE_LENGTH);
-    bottomPiece = track[bottomPieceIdx];
-
-    bottomPieceY = 0;
-    for (i = 0; i < bottomPieceIdx; i += 1) {
-      bottomPieceY += track[i].elevation - offsets.elevation;
-    }
-
-    cameraY = (bottomPiece.elevation - offsets.elevation) * interpolationCoef + bottomPieceY + 1;
-
-    return cameraY;
-  }
-
-  function interpolateTrackElevation(hillFeature: TrackGenerationFeature) {
-    return (
-      Math.sin(
-        (
-          Math.PI *
-          ((hillFeature.stepsDone + 1) / hillFeature.steps)
-        ) - (Math.PI / 2)
-      ) * hillFeature.change
-    ) - (
-      Math.sin(
-        (
-          Math.PI *
-          (hillFeature.stepsDone / hillFeature.steps)
-        ) - (Math.PI / 2)
-      ) * hillFeature.change
-    );
-  }
-
-  function interpolateElevationOffset(last: number, now: number, cameraZ: number) {
-    let interpolationCoef = (cameraZ % TRACK_PIECE_LENGTH) / TRACK_PIECE_LENGTH;
-    return ((now - last) * interpolationCoef + last);
-  }
-
-  function build3DTrack(track: TrackPiece[], offsets: TrackOffsets) {
-    let
-      threeDTrack = [] as Polygon[],
-      combinedElevation = 0,
-      combinedTurn = 0,
-      combinedXOffset = 0,
-      piece: TrackPiece,
-      feature: TrackFeature,
-      featureXOffset: number,
-      i: number, j: number, y1: number, y2: number, z1: number, z2: number,
-      x1Offset: number, x2Offset: number;
-
-    combinedTurn    += offsets.turn;
-    combinedXOffset += offsets.turn;
-
-    for (i = 0; i < track.length; i += 1) {
-      combinedElevation += track[i].elevation - offsets.elevation;
-
-      if (i > Math.floor(stepsMoved / STEPS_PER_PIECE)) {
-        combinedTurn += track[i].turn;
-        combinedXOffset += combinedTurn;
-      }
-    }
-
-    for (i = (track.length - 1); i >= 0; i -= 1) {
-      piece = track[i];
-
-      x1Offset = combinedXOffset - combinedTurn;
-      x2Offset = combinedXOffset;
-      y1 = combinedElevation - (piece.elevation - offsets.elevation);
-      y2 = combinedElevation;
-      z1 = i * TRACK_PIECE_LENGTH + 1;
-      z2 = (i + 1) * TRACK_PIECE_LENGTH + 1;
-
-      // Background
-      threeDTrack.push({
-        points: [
-          [-1000, y1, z1],
-          [ 1000, y1, z1],
-          [ 1000, y2, z2],
-          [-1000, y2, z2]
-        ],
-        color: piece.background
-      });
-
-      // Features
-      featureXOffset = 0;
-
-      for (j = 0; j < piece.features.length; j += 1) {
-        feature = piece.features[j];
-
-        threeDTrack.push({
-          points: [
-            [
-              -1 + (featureXOffset * 2) + x1Offset,
-              y1, z1
-            ], [
-              -1 + ((featureXOffset + feature.width) * 2) + x1Offset,
-              y1, z1
-            ], [
-              -1 + ((featureXOffset + feature.width) * 2) + x2Offset,
-              y2, z2
-            ], [
-              -1 + (featureXOffset * 2) + x2Offset,
-              y2, z2
-            ]
-          ],
-          color: feature.color
-        });
-
-        featureXOffset += feature.width;
-      }
-
-      combinedElevation -= (piece.elevation - offsets.elevation);
-      combinedXOffset   -= combinedTurn;
-      combinedTurn      -= piece.turn;
-    }
-
-    return threeDTrack;
-  }
-
-  function update() {
-    let
-      invisiblePieces: number,
-      piecesToRemove: number,
-      i: number,
-      lastPieceEven: boolean,
-      offsets: TrackOffsets;
-
-    // Advance position
-    stepsMoved += 1;
-    camera[2] = stepsMoved * SPEED;
-
-    // Remove old track pieces, add new ones
-    piecesToRemove = Math.floor(stepsMoved / STEPS_PER_PIECE) - 2;
-    lastPieceEven = track[track.length - 1].even;
-
-    if (piecesToRemove > 0) {
-      track.splice(0, piecesToRemove);
-
-      for (i = 0; i < piecesToRemove; i += 1) {
-        lastPieceEven = !lastPieceEven;
-        track.push(generateTrackPiece(lastPieceEven, trackFeatures));
-      }
-
-      stepsMoved -= piecesToRemove * STEPS_PER_PIECE;
-      camera[2] = stepsMoved * SPEED;
-    }
-
-    offsets = {
-      elevation: 0,
-      turn: 0
-    };
-
-    // Update elevation offset
-    offsets.elevation = interpolateElevationOffset(
-      (track[0].elevation + track[1].elevation + track[2].elevation + track[3].elevation + track[4].elevation) / 5,
-      (track[1].elevation + track[2].elevation + track[3].elevation + track[4].elevation + track[5].elevation) / 5,
-      camera[2]
-    );
-
-    // Update turn offset
-    invisiblePieces = Math.floor(stepsMoved / STEPS_PER_PIECE);
-    offsets.turn = (1 - ((stepsMoved % STEPS_PER_PIECE) / STEPS_PER_PIECE)) * track[invisiblePieces].turn;
-
-    // Interpolate camera Y
-    camera[1] = interpolateCameraY(camera[2], offsets);
-
-    // Build 3D track
-    return build3DTrack(track, offsets);
-  }
-
-  function render(polygons: Polygon[]) {
-    let
-      polygon: Polygon,
-      point: number[],
-      i: number, j: number;
-
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    for (i = 0; i < polygons.length; i += 1) {
-      polygon = polygons[i];
-
-      for (j = 0; j < polygon.points.length; j += 1) {
-        point = transformToCamera(polygon.points[j].slice(0));
-
-        pointsArray[j * 3]     = point[0];
-        pointsArray[j * 3 + 1] = point[1];
-        pointsArray[j * 3 + 2] = point[2];
-      }
-
-      gl.uniform4f(
-        colorLocation,
-        polygon.color[0], polygon.color[1], polygon.color[2], 1
-      );
-
-      gl.uniformMatrix4fv(
-        projectionLocation, false,
-        GL.perspectiveMatrix()
-      );
-
-      gl.bufferData(gl.ARRAY_BUFFER, pointsArray, gl.STATIC_DRAW);
-      gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
-    }
-
-    ctx.clearRect(0, 0, realCanvas.width, realCanvas.height);
-    ctx.drawImage(canvas, 0, 0, canvas.width, canvas.height,
-                  0, 0, realCanvas.width, realCanvas.height);
+    realCtx.clearRect(0, 0, realCanvas.width, realCanvas.height);
+    realCtx.drawImage(canvas, 0, 0, canvas.width, canvas.height,
+      0, 0, realCanvas.width, realCanvas.height);
   }
 
   export function playPause() {
@@ -412,10 +55,7 @@ namespace App {
   }
 
   export function nextFrame() {
-    let threeDTrack: Polygon[];
-
-    threeDTrack = update();
-    render(threeDTrack);
+    render();
 
     if (!paused) {
       window.requestAnimationFrame(nextFrame);
@@ -423,32 +63,10 @@ namespace App {
   }
 
   // Initialize stuff
-  realCanvas         = document.getElementById('game') as HTMLCanvasElement;
-  ctx                = realCanvas.getContext('2d');
-  canvas             = document.createElement('canvas');
-  gl                 = GL.initWebGL(canvas);
-  shaderProgram      = GL.initShaders(gl);
-  positionLocation   = gl.getAttribLocation(shaderProgram, 'a_position');
-  colorLocation      = gl.getUniformLocation(shaderProgram, 'u_color');
-  projectionLocation = gl.getUniformLocation(shaderProgram, 'u_projection');
-
-  buffer = gl.createBuffer();
-
-  gl.clearColor(0, 0, 0, 0);
-  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-  gl.enableVertexAttribArray(positionLocation);
-  gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
-
-  // Initial track features
-  trackFeatures = {
-    hill: { steps: 0, stepsDone: 0, change: 0 },
-    turn: { steps: 0, stepsDone: 0, change: 0 }
-  };
-
-  // Initial track
-  for (i = 0; i < MAX_PIECES_VISIBLE; i += 1) {
-    track.push(generateTrackPiece(i % 2 === 0, trackFeatures));
-  }
+  realCanvas = document.getElementById('game') as HTMLCanvasElement;
+  realCtx    = realCanvas.getContext('2d');
+  canvas     = document.createElement('canvas');
+  ctx        = canvas.getContext('2d');
 
   // Resize the canvas to fill browser window dynamically
   window.addEventListener('resize', resizeCanvas, false);
