423bc4fd95cd2035731f4414cbd55e7e25e522be
Mantas Norvaiša <mntnorv@gmail.com>
2016-06-21 23:32:55 +0300

    Water stuff

--- js/app.js
+++ js/app.js
@@ -1,48 +1,82 @@
 var App;
 (function (App) {
-    var MIN_WIDTH = 240;
-    var MIN_HEIGHT = 135;
+    var POINTS_X = 20;
+    var POINTS_Y = 20;
+    var Point3 = (function () {
+        function Point3() {
+        }
+        return Point3;
+    }());
+    var Point2 = (function () {
+        function Point2() {
+        }
+        return Point2;
+    }());
     var Polygon = (function () {
         function Polygon() {
         }
         return Polygon;
     }());
-    var realCanvas, realCtx, canvas, ctx, polygons, pixel, paused = false;
+    var canvas, ctx, waterPoints, paused = false, i, j;
     function resizeCanvas() {
-        var pixelRatio;
-        pixelRatio = Math.min(window.innerWidth / MIN_WIDTH, window.innerHeight / MIN_HEIGHT);
-        canvas.width = Math.ceil(window.innerWidth / pixelRatio);
-        canvas.height = Math.ceil(window.innerHeight / pixelRatio);
-        realCanvas.width = window.innerWidth;
-        realCanvas.height = window.innerHeight;
-        realCtx.mozImageSmoothingEnabled = false;
-        realCtx.webkitImageSmoothingEnabled = false;
-        realCtx.msImageSmoothingEnabled = false;
-        realCtx.imageSmoothingEnabled = false;
-        ctx.translate(0.5, 0.5);
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
     }
-    function drawLine(fromP, toP) {
-        var x1 = fromP[0], y1 = fromP[1], x2 = toP[0], y2 = toP[1], xMin = Math.min(x1, x2), xMax = Math.max(x1, x2), x, y;
-        for (x = xMin; x < xMax; x++) {
-            y = (((y2 - y1) / (x2 - x1)) * (x - x1)) + y1;
-            pixel.data[0] = 0;
-            pixel.data[1] = 0;
-            pixel.data[2] = 0;
-            pixel.data[3] = 255;
-            ctx.putImageData(pixel, x, y);
-        }
+    function projectPoint3(point) {
+        var point2;
+        point2 = {
+            x: point.x * canvas.width,
+            y: point.y * canvas.height
+        };
+        point2.y -= (point.z * canvas.height) / POINTS_Y;
+        return point2;
     }
     function render() {
-        var polygon, i, j;
+        var points2D, polygons, i, j, point, color;
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        for (i = 0; i < polygons.length; i++) {
-            polygon = polygons[i];
-            for (j = 0; j < (polygon.points.length - 1); j++) {
-                drawLine(polygon.points[j], polygon.points[j + 1]);
+        points2D = [];
+        for (i = 0; i < waterPoints.length; i++) {
+            points2D.push(projectPoint3(waterPoints[i]));
+        }
+        polygons = [];
+        for (i = 0; i < (POINTS_Y - 1); i++) {
+            for (j = 0; j < (POINTS_X - 1); j++) {
+                color = Math.round((((i * POINTS_Y) + j) / (POINTS_X * POINTS_Y)) * 255).toString(16);
+                if (color.length < 2) {
+                    color = '0' + color;
+                }
+                polygons.push({
+                    points: [
+                        points2D[i * POINTS_X + j],
+                        points2D[i * POINTS_X + (j + 1)],
+                        points2D[(i + 1) * POINTS_X + j]
+                    ],
+                    color: '#0000' + color
+                });
+                polygons.push({
+                    points: [
+                        points2D[i * POINTS_X + (j + 1)],
+                        points2D[(i + 1) * POINTS_X + j],
+                        points2D[(i + 1) * POINTS_X + (j + 1)]
+                    ],
+                    color: '#0000' + color
+                });
             }
         }
-        realCtx.clearRect(0, 0, realCanvas.width, realCanvas.height);
-        realCtx.drawImage(canvas, 0, 0, canvas.width, canvas.height, 0, 0, realCanvas.width, realCanvas.height);
+        for (i = 0; i < polygons.length; i++) {
+            ctx.fillStyle = polygons[i].color;
+            ctx.beginPath();
+            for (j = 0; j < polygons[i].points.length; j++) {
+                point = polygons[i].points[j];
+                if (j === 0) {
+                    ctx.moveTo(point.x, point.y);
+                }
+                else {
+                    ctx.lineTo(point.x, point.y);
+                }
+            }
+            ctx.fill();
+        }
     }
     function playPause() {
         paused = !paused;
@@ -59,30 +93,19 @@ var App;
     }
     App.nextFrame = nextFrame;
     // Initialize stuff
-    realCanvas = document.getElementById('game');
-    realCtx = realCanvas.getContext('2d');
-    canvas = document.createElement('canvas');
+    canvas = document.getElementById('game');
     ctx = canvas.getContext('2d');
-    pixel = ctx.createImageData(1, 1);
-    // Add text
-    polygons = [];
-    polygons.push({
-        points: [
-            [10, 10],
-            [10, 40],
-            [20, 40],
-            [20, 20],
-            [25, 30],
-            [30, 20],
-            [30, 40],
-            [40, 40],
-            [40, 10],
-            [30, 10],
-            [25, 20],
-            [20, 10]
-        ],
-        color: '#000'
-    });
+    // Initialize water
+    waterPoints = [];
+    for (i = 0; i < POINTS_Y; i++) {
+        for (j = 0; j < POINTS_X; j++) {
+            waterPoints.push({
+                x: (j / (POINTS_X - 2) + ((i % 2) * (1 / POINTS_X))) - (1 / POINTS_X),
+                y: i / (POINTS_Y - 2),
+                z: Math.random()
+            });
+        }
+    }
     // Resize the canvas to fill browser window dynamically
     window.addEventListener('resize', resizeCanvas, false);
     // Start
--- src/app.ts
+++ src/app.ts
@@ -1,80 +1,106 @@
 namespace App {
-  const MIN_WIDTH  = 240;
-  const MIN_HEIGHT = 135;
+  const POINTS_X = 20;
+  const POINTS_Y = 20;
+
+  class Point3 {
+    x: number;
+    y: number;
+    z: number;
+  }
+
+  class Point2 {
+    x: number;
+    y: number;
+  }
 
   class Polygon {
-    points: [number, number][];
+    points: Point2[];
     color: string;
   }
 
   let
-    realCanvas: HTMLCanvasElement,
-    realCtx: CanvasRenderingContext2D,
     canvas: HTMLCanvasElement,
     ctx: CanvasRenderingContext2D,
-    polygons: Polygon[],
-    pixel: ImageData,
-    paused = false;
+    waterPoints: Point3[],
+    paused = false,
+    i: number, j: number;
 
   function resizeCanvas() {
-    let pixelRatio: number;
-
-    pixelRatio = Math.min(
-      window.innerWidth  / MIN_WIDTH,
-      window.innerHeight / MIN_HEIGHT
-    );
-
-    canvas.width  = Math.ceil(window.innerWidth  / pixelRatio);
-    canvas.height = Math.ceil(window.innerHeight / pixelRatio);
-
-    realCanvas.width  = window.innerWidth;
-    realCanvas.height = window.innerHeight;
-
-    (realCtx as any).mozImageSmoothingEnabled = false;
-    (realCtx as any).webkitImageSmoothingEnabled = false;
-    (realCtx as any).msImageSmoothingEnabled = false;
-    (realCtx as any).imageSmoothingEnabled = false;
-
-    ctx.translate(0.5, 0.5);
+    canvas.width  = window.innerWidth;
+    canvas.height = window.innerHeight;
   }
 
-  function drawLine(fromP: [number, number], toP: [number, number]) {
-    let
-      x1   = fromP[0], y1 = fromP[1],
-      x2   = toP[0],   y2 = toP[1],
-      xMin = Math.min(x1, x2),
-      xMax = Math.max(x1, x2),
-      x: number, y: number;
+  function projectPoint3(point: Point3) {
+    let point2: Point2;
 
-    for (x = xMin; x < xMax; x++) {
-      y = (((y2 - y1) / (x2 - x1)) * (x - x1)) + y1;
-      pixel.data[0] = 0;
-      pixel.data[1] = 0;
-      pixel.data[2] = 0;
-      pixel.data[3] = 255;
-      ctx.putImageData(pixel, x, y);
-    }
+    point2 = {
+      x: point.x * canvas.width,
+      y: point.y * canvas.height
+    };
+
+    point2.y -= (point.z * canvas.height) / POINTS_Y;
+
+    return point2;
   }
 
   function render() {
-    let polygon: Polygon, i: number, j: number;
+    let
+      points2D: Point2[],
+      polygons: Polygon[],
+      i: number, j: number, point: Point2, color: string;
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    for (i = 0; i < polygons.length; i++) {
-      polygon = polygons[i];
+    points2D = [];
+    for (i = 0; i < waterPoints.length; i++) {
+      points2D.push(projectPoint3(waterPoints[i]));
+    }
 
-      for (j = 0; j < (polygon.points.length - 1); j++) {
-        drawLine(
-          polygon.points[j],
-          polygon.points[j + 1]
-        );
+    polygons = [];
+    for (i = 0; i < (POINTS_Y - 1); i++) {
+      for (j = 0; j < (POINTS_X - 1); j++) {
+        color = Math.round((((i * POINTS_Y) + j) / (POINTS_X * POINTS_Y)) * 255).toString(16);
+
+        if (color.length < 2) {
+          color = '0' + color;
+        }
+
+        polygons.push({
+          points: [
+            points2D[i * POINTS_X + j],
+            points2D[i * POINTS_X + (j + 1)],
+            points2D[(i + 1) * POINTS_X + j]
+          ],
+          color: '#0000' + color
+        });
+
+        polygons.push({
+          points: [
+            points2D[i * POINTS_X + (j + 1)],
+            points2D[(i + 1) * POINTS_X + j],
+            points2D[(i + 1) * POINTS_X + (j + 1)]
+          ],
+          color: '#0000' + color
+        });
       }
     }
 
-    realCtx.clearRect(0, 0, realCanvas.width, realCanvas.height);
-    realCtx.drawImage(canvas, 0, 0, canvas.width, canvas.height,
-      0, 0, realCanvas.width, realCanvas.height);
+    for (i = 0; i < polygons.length; i++) {
+      ctx.fillStyle = polygons[i].color;
+      ctx.beginPath();
+
+      for (j = 0; j < polygons[i].points.length; j++) {
+        point = polygons[i].points[j];
+
+        if (j === 0) {
+          ctx.moveTo(point.x, point.y);
+        } else {
+          ctx.lineTo(point.x, point.y);
+        }
+      }
+
+      ctx.fill();
+    }
   }
 
   export function playPause() {
@@ -94,33 +120,21 @@ namespace App {
   }
 
   // Initialize stuff
-  realCanvas = document.getElementById('game') as HTMLCanvasElement;
-  realCtx    = realCanvas.getContext('2d');
-  canvas     = document.createElement('canvas');
-  ctx        = canvas.getContext('2d');
+  canvas = document.getElementById('game') as HTMLCanvasElement;
+  ctx    = canvas.getContext('2d');
 
-  pixel = ctx.createImageData(1, 1);
+  // Initialize water
+  waterPoints = [];
 
-  // Add text
-  polygons = [];
-
-  polygons.push({
-    points: [
-      [10, 10],
-      [10, 40],
-      [20, 40],
-      [20, 20],
-      [25, 30],
-      [30, 20],
-      [30, 40],
-      [40, 40],
-      [40, 10],
-      [30, 10],
-      [25, 20],
-      [20, 10]
-    ],
-    color: '#000'
-  });
+  for (i = 0; i < POINTS_Y; i++) {
+    for (j = 0; j < POINTS_X; j++) {
+      waterPoints.push({
+        x: (j / (POINTS_X - 2) + ((i % 2) * (1 / POINTS_X))) - (1 / POINTS_X),
+        y: i / (POINTS_Y - 2),
+        z: Math.random()
+      });
+    }
+  }
 
   // Resize the canvas to fill browser window dynamically
   window.addEventListener('resize', resizeCanvas, false);
